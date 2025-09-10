66cd141e1d9187e69ef31cf6bb15f6d4797dab87
Chrisimana <christianyusni@gmail.com>
2025-09-10 20:46:47 +0800

    Add files via upload

---
+++ 005 Konversi Tipe Data/main.py
@@ -0,0 +1,9 @@
+print(".::Program Luas Persegi Panjang::.\n")
+
+panjang = input("Masukkan panjang : ")
+lebar = input("Masukkan lebar : ")
+panjang = int(panjang)
+lebar = int(lebar)
+luas = panjang*lebar
+
+print("Luas Persegi Panjang : ", luas)
