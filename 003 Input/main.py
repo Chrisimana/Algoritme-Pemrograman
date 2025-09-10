4fbbe7fa67d5dc0f276ae30d59ce5673e5ad3bb8
Chrisimana <christianyusni@gmail.com>
2025-09-10 20:46:16 +0800

    Add files via upload

---
+++ 003 Input/main.py
@@ -0,0 +1,5 @@
+umur = input("Umur anda berapa ? ")
+tinggi = input("Tinggi anda ? ")
+weight = input("Kalau berat bedan ? ")
+
+print(f"Jadi, umur anda {umur} tahun, tinggi {tinggi} cm.dan berat {weight} kg.")
