9eac72dc1179bdeec5ad886ee74138796a4e20e1
Chrisimana <christianyusni@gmail.com>
2025-09-10 20:46:04 +0800

    Add files via upload

---
+++ 002 Print/main.py
@@ -0,0 +1,24 @@
+jum_motor = 3
+jum_rumah = 3
+umur_saya = 18
+rambut_saya = "Hitam"
+print("Saya punya", jum_motor, "motor dan", jum_rumah, "rumah")
+print(f"umur saya {umur_saya} tahun dan saya punya rambut warna{rambut_saya}")
+jenis_manusia = 2
+
+biner = "tau bilangan biner"
+not_biner = "tidak tau bilangan biner"
+x = f"Di dunia ini ada {jenis_manusia} jenis manusia yaitu yang {biner}dan yang {not_biner}"
+print(x)
+
+manusia = False
+who = "program"
+teks = "Lalu saya siapa ? Apakah saya manusia ?! {} , lalu saya apa ?{}"
+print(teks.format(manusia, who))
+
+print("""
+Pergi wisata ke kota solo
+Singgah sebentar di kota bantul
+Sungguh malang nasib si jomblo
+Setiap malam hanya memeluk dengkul
+""")
