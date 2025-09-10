246890e7cbcfbe2f034cdeba0ab37041f64476b5
Chrisimana <christianyusni@gmail.com>
2025-09-10 20:46:29 +0800

    Add files via upload

---
+++ 004 Aritmatika/main.py
@@ -0,0 +1,20 @@
+# pustaka math berisi fungsi-fungsi dan konstanta
+# matematika
+import math
+
+# Persegi Panjang
+panjang = 1.10
+lebar = 2.20
+luas = panjang * lebar
+print("Luas persegi panjang : " , luas)
+
+#Luas Lingkaran
+jari2 = 2.20
+luas = math.pi * jari2**2
+print("Luas lingkaran : " , luas)
+
+#Luas Permukaan Tabung
+jari2Tabung = 2.20
+tinggiTabung = 10
+luasPermukaan = 2*math.pi*jari2Tabung*(jari2Tabung+tinggiTabung)
+print("Luas Permukaan Tabung : " , luasPermukaan)
