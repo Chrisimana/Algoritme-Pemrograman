45bf306ad1c6f3215e411d3b7bf0380f5c715a75
Chrisimana <christianyusni@gmail.com>
2025-09-10 20:45:51 +0800

    Add files via upload

---
+++ 001 Penggunaan Variabel/main.py
@@ -0,0 +1,19 @@
+#Contoh definisi variabel untuk menampung karakter
+kalimat = "Halo semua, saya adalah program komputer"
+print(kalimat)
+
+#Contoh definisi variabel untuk menampung bilangan bulat
+bil_bulat = 7800
+print(bil_bulat)
+
+#Contoh definisi variabel untuk menampung bilangan desimal
+bil_desimal = 10.087
+print(bil_desimal)
+
+#Contoh definisi dua variabel sekaligus dengan integer
+a = b = 123
+print(a, b)
+
+#Contoh lainnya, nama variabel dapat mengandung angka
+kalimat2 = "Bye bye..."
+print(kalimat2)
