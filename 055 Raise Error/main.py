1f29b5ec955eb8f87950ccd15728e301391024c8
Chrisimana <christianyusni@gmail.com>
2025-09-12 17:26:09 +0800

    Add files via upload

---
+++ 055 Raise Error/main.py
@@ -0,0 +1,8 @@
+def hapusProduk(akses, produk):
+    if akses != "admin":
+        raise AssertionError("Anda harus punya akses admin untuk bisa menghapus barang")
+
+    if produk not in ["tas", "jam", "baju", "celana", "buku"]:
+        raise ValueError("Produk tidak ada")
+
+hapusProduk("pegawai", "komputer")
